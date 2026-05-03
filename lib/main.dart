82b47355f5506771382b825f30f8a9af6759a7d2
Mahmoud Rabie <mahmoudrabie.mr23@gmail.com>
2026-05-03 14:32:04 +0300

    provide cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:notes_app/constant.dart';
+import 'package:notes_app/cubits/add%20note/add_note_cubit.dart';
 import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/views/edit_note_view.dart';
 import 'package:notes_app/views/notes_view.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async {
   await Hive.initFlutter();
@@ -17,12 +19,20 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      routes: {EditNoteView.id: (context) => EditNoteView()},
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(brightness: Brightness.dark, 
-      fontFamily: 'Poppins',),
-      home: NotesView(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => AddNoteCubit(),
+        ),
+      ],
+      
+      child: MaterialApp(
+        routes: {EditNoteView.id: (context) => EditNoteView()},
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(brightness: Brightness.dark, 
+        fontFamily: 'Poppins',),
+        home: NotesView(),
+      ),
     );
   }
 }
